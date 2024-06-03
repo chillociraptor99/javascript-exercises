9ad313cde44034275578e9101240a39c565adc6d
chillociraptor <169479308+chillociraptor99@users.noreply.github.com>
2024-06-03 15:18:10 -0500

    Create basic removal function
    Created a basic removal function working for tests 1 and 2

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
 const removeFromArray = function() {
+        const args = Array.from(arguments);
+        array = args[0];
+        removeValues = args.toSpliced(0,1);
+        newArray = Array.from(array);
+        
+        for (var i = removeValues.length - 1; i >= 0; i--) {
+            let position = newArray.indexOf(removeValues[i]);
+            finalArray = newArray.toSpliced(position, 1);
+            console.log("Array after: " + finalArray);
+            newArray = finalArray;
+        }
+        return finalArray;
 };
 
 // Do not edit below this line
