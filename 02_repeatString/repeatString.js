d5a263a2b5ce84035ca57035e0ae543e6655e515
chillociraptor <169479308+chillociraptor99@users.noreply.github.com>
2024-06-03 09:38:53 -0500

    Create working loop
    Created working loop to accept test input.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+let repeated = "";
 
+const repeatString = function(word,num) {
+    repeated = "";
+    for (let i = 0; i < num; i++) {
+     repeated += word;
+    }
+    if (num < 0) {
+        repeated = "ERROR";
+    }
+    return repeated;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
