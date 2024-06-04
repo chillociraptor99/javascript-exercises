ebef7e56fc27c81fb016cc833845e4e4a83b4475
chillociraptor <169479308+chillociraptor99@users.noreply.github.com>
2024-06-04 12:27:50 -0500

    Pass all tests until integer checks
    Corrected function to pass all tests up until integer checks.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,18 +1,24 @@
 const sumAll = function () {
     let args = Array.from(arguments);
-    args = args.sort();
-    let counter = ((args[1] - args[0]) - 1);
-    console.log("Counter: " + counter);
-    let midSums = 0;
-    while (counter >= 1) {
-        console.log("Args last: " + args[1]);
-    midSums += (args[1] - counter);
-    counter--;
+    if (isNaN(args[0]) || isNaN(args[1])) {
+        return "ERROR";
+    } else if (args[0] < 0 || args[1] < 0) {
+        return "ERROR";
+    } else {
+        args = args.sort();
+        let counter = ((args[1] - args[0]) - 1);
+        console.log("Counter: " + counter);
+        let midSums = 0;
+        while (counter >= 1) {
+            console.log("Args last: " + args[1]);
+        midSums += (args[1] - counter);
+        counter--;
+        }
+        console.log("Mid Sums: " + midSums);
+        allSum = (midSums + args[0] + args[1]);
+        console.log("Final sum: " + allSum);
+        return allSum;
     }
-    console.log("Mid Sums: " + midSums);
-    allSum = (midSums + args[0] + args[1]);
-    console.log("Final sum: " + allSum);
-    return allSum;
   };
 
 // Do not edit below this line
