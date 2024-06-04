f85b38107bedac142978271151ee52f2433c68dd
chillociraptor <169479308+chillociraptor99@users.noreply.github.com>
2024-06-04 12:19:48 -0500

    Complete first test
    Successfully built logic to past first sum test.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
-};
+const sumAll = function () {
+    let args = Array.from(arguments);
+    args = args.sort();
+    let counter = ((args[1] - args[0]) - 1);
+    console.log("Counter: " + counter);
+    let midSums = 0;
+    while (counter >= 1) {
+        console.log("Args last: " + args[1]);
+    midSums += (args[1] - counter);
+    counter--;
+    }
+    console.log("Mid Sums: " + midSums);
+    allSum = (midSums + args[0] + args[1]);
+    console.log("Final sum: " + allSum);
+    return allSum;
+  };
 
 // Do not edit below this line
 module.exports = sumAll;
