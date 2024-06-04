39ef8361bf0e0cc0a244581aba4f1f78fdb6787f
chillociraptor <169479308+chillociraptor99@users.noreply.github.com>
2024-06-04 13:22:34 -0500

    Clean up
    Cleaned up code and if checks

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,20 +1,17 @@
 const leapYears = function(year) {
-    let remaindFour = year % 4;
-    let remaindFourhund = year % 400;
-    console.log(remaindFour + remaindFourhund);
     if (year % 4 === 0) {
-        if (year % 100 === 0) {
-            if (year % 400 === 0) {
-                return true;
-            } else {
-                return false;
-            }
+        if ((year % 100 === 0) && (year % 400 === 0)) {
+            return true;
+        } else if (year % 100 === 0) {
+            return false;
         }
-        return true;
+        else {
+            return true;
+        }
     } else {
         return false;
     }
-};
+}
 
 // Do not edit below this line
 module.exports = leapYears;
