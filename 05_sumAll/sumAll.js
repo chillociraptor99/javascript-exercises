222479b27ce41d593b80f91c562d7cc8efb2c4f6
chillociraptor <169479308+chillociraptor99@users.noreply.github.com>
2024-06-04 12:31:40 -0500

    Pass all tests
    Corrected function to pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,22 +1,18 @@
 const sumAll = function () {
     let args = Array.from(arguments);
-    if (isNaN(args[0]) || isNaN(args[1])) {
+    if (!Number.isInteger(args[0]) || !Number.isInteger(args[1])) {
         return "ERROR";
     } else if (args[0] < 0 || args[1] < 0) {
         return "ERROR";
     } else {
         args = args.sort();
         let counter = ((args[1] - args[0]) - 1);
-        console.log("Counter: " + counter);
         let midSums = 0;
         while (counter >= 1) {
-            console.log("Args last: " + args[1]);
         midSums += (args[1] - counter);
         counter--;
         }
-        console.log("Mid Sums: " + midSums);
         allSum = (midSums + args[0] + args[1]);
-        console.log("Final sum: " + allSum);
         return allSum;
     }
   };
