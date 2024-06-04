30753d0d1c51de1e81a079b2a7cb380a7b0ef426
chillociraptor <169479308+chillociraptor99@users.noreply.github.com>
2024-06-04 13:13:09 -0500

    Create working prototype
    Created working prototype that passes all tests.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let remaindFour = year % 4;
+    let remaindFourhund = year % 400;
+    console.log(remaindFour + remaindFourhund);
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
