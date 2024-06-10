1d7e2e9b1ff8b04132840d8855475fc33f2cd595
chillociraptor <169479308+chillociraptor99@users.noreply.github.com>
2024-06-10 13:23:37 -0500

    Pass tests
    Created functions to pass all tests.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let noPunc = word.match(/[^_\W]+/g).join('');
+    let lowerC = noPunc.toLowerCase()
+    strArray = Array.from(lowerC);
+    revArray = strArray.toReversed();
+    revvedArray = revArray.join('');
+    console.log(lowerC + "=" + revvedArray);
+    return revvedArray === lowerC;
 };
 
 // Do not edit below this line
