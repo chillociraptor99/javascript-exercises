b7ee2fe2e922e3de9102157a699ba50b39d0d27a
chillociraptor <169479308+chillociraptor99@users.noreply.github.com>
2024-06-10 12:52:07 -0500

    Fulfill tests
    Completed requested tests.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	let sum = num1 + num2;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  let sub = num1 - num2;
+  return sub;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2; 
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
+  }
 
 // Do not edit below this line
 module.exports = {
